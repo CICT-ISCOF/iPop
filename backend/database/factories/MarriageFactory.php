f29e5ae5892f20dc1e20705ba7c1946fb9bca76d
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2020-08-10 08:53:39 +0800

    Updated Schema

---
+++ backend/database/factories/MarriageFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Marriage;
+use Faker\Generator as Faker;
+
+$factory->define(Marriage::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ backend/database/migrations/2020_08_09_194137_create_logs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->text('action');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
