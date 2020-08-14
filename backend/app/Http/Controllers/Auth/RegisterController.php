1ee33500efac5a6582d51ffd771845892322efe2
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2020-08-14 17:21:54 +0800

    .env

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -13,23 +13,12 @@ class RegisterController extends Controller
     public function register(Request $request)
     {
         $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
         return $this->create($data);
     }
 
     private function create(array $data)
     {
-        return User::create([
-            // 'email' => $data['email'],
-            'username' => $data['username'],
-            'password' => Hash::make($data['password']),
-            'fullname' => $data['fullname'],
-            'barangay' => $data['barangay'],
-            'municipality' => $data['municipality'],
-            'role' => $data['role'],
-            'pin' => isset($data['pin']) ? $data['pin'] : null,
-            'question' => isset($data['question']) ? $data['question'] : null,
-            'answer' => isset($data['answer']) ? $data['answer'] : null,
-            'district' => $data['district'],
-        ]);
+        return User::create($data);
     }
 }
