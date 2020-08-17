34600583bf8d5fc298bbb08e904ca33c2ee246fe
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2020-08-18 06:19:07 +0800

    Update

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/auth')->group(function () {
     Route::post('/login', 'Auth\LoginController@authenticate');
-    Route::post('/register', 'Auth\RegisterController@register');
 });
 
 Route::options('/logs/visit', 'LogController@visit');
