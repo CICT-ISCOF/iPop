2edc7219175d3d218fa0a298204f054238ff5071
Jamel <jamelyassin84@gmail.com>
2020-11-17 20:19:54 +0800

    Finished Services

--- frontend/src/app/content/general-public/services-offered/pdi-services/pdi-services.component.ts
+++ frontend/src/app/content/general-public/services-offered/pdi-services/pdi-services.component.ts
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2'
+import { ServicesOfferedService } from './../services-offered.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PDIServicesComponent implements OnInit {
 
-  constructor() { }
+    
+	constructor(
+		private ServicesOfferedService : ServicesOfferedService
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+
+	}
+
+	type = 'rpfp'
+
+	data = {
+		service:'',
+		type:'rpfp'
+	}
+
+	services = [		
+		'Gender-Responsive PopDev Planning',
+		'PopDev Workshop'
+	]
+
+	create(){
+		this.ServicesOfferedService.create(this.data).subscribe(data => {
+
+		})
+	}
+
+	retrieve(){
+		this.ServicesOfferedService.retrieve(this.data.type).subscribe(data => {
+
+		})
+	}
+
+	update(id){
+		this.ServicesOfferedService.update(this.data.type, id).subscribe(data => {
+
+		})
+	}
+
+	deleteService(id){	
+		Swal.fire({
+			title: 'Are you sure you want to remove this Service?',		
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonText: 'Remove Service',
+			cancelButtonText: 'Nope'
+		  }).then((result) => {
+			if (result.value) {
+				this.ServicesOfferedService.delete(this.data.type, id).subscribe(data => {
+
+				})		
+			} 
+		})
+	}
+
+
+	acitveServices = {}
+
+	wantsToAdd = false
+
+	toggleServices(service_id){		
+		this.acitveServices[service_id] == true ?  this.acitveServices[service_id] = false : this.acitveServices[service_id] = true	
+		console.log(this.acitveServices[service_id])		
+	}
 
 }
--- frontend/src/app/content/general-public/services-offered/services-offered.service.ts
+++ frontend/src/app/content/general-public/services-offered/services-offered.service.ts
@@ -1,9 +1,54 @@
+import { BaseAPIService } from './../../../base-api.service';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesOfferedService {
 
-  constructor() { }
+	constructor(
+		private http : HttpClient,
+		private BaseAPIService : BaseAPIService
+	) {
+		if(localStorage.getItem('user-data') && localStorage.getItem('user-data') != undefined){
+			this.user = JSON.parse(localStorage.getItem('user-data'))
+			this.token = this.user.token
+		}
+	 }
+
+	user:any = ''
+	token:any = ''
+
+	baseURL = 	this.BaseAPIService.baseURL + '/users'
+	headers = 	new HttpHeaders({
+					'Accept':'application/json',
+					'Authorization' : 'Bearer '+ this.token,
+					'Content-Type':[]
+				})	
+
+	private page = new Subject<any>();
+
+	create(string){
+		const url = this.baseURL
+		return this.http.post<any>(url, string , {headers:this.headers})
+	}
+
+	retrieve(type){
+		const url = this.baseURL 
+		return this.http.get<any>(url,{headers:this.headers})
+	}
+
+	update(string, id){
+		const url = this.baseURL + id
+		return this.http.put<any>(url, string ,{headers:this.headers})
+	}
+
+	delete(type, id){
+		const url = this.baseURL + id
+		return this.http.delete<any>(url ,{headers:this.headers})
+	}
 }
