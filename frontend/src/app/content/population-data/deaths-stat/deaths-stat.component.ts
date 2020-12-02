512321d9b6deef1de170776c8db60f00ec0d29af
Jamel <jamelyassin84@gmail.com>
2020-12-02 20:01:35 +0800

    Demographic/Statistics-Changes

--- frontend/src/app/content/population-data/deaths-stat/deaths-stat.component.ts
+++ frontend/src/app/content/population-data/deaths-stat/deaths-stat.component.ts
@@ -7,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeathsStatComponent implements OnInit {
 
-	constructor() { }
+  constructor() { }
+  
+	MONTHbarChartOptions = {
+		scaleShowVerticalLines: false,
+		responsive: true
+	};
+	MONTHbarChartLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	MONTHbarChartType = 'bar';
+	MONTHbarChartLegend = true;
+	MONTHbarChartData = [
+		{data: [65, 59, 80, 81, 14, 55, 61, 11, 14, 55, 61, 61], label: 'Deaths By Months'},
+	
+	];
+
+
+	// -------------
+
+	DEATHRATEbarChartOptions = {
+		scaleShowVerticalLines: false,
+		responsive: true
+	};
+	DEATHRATEbarChartLabels = ['2018', '2019', '2020',];
+	DEATHRATEbarChartType = 'line';
+	DEATHRATEbarChartLegend = true;
+	DEATHRATEbarChartData = [
+		{data: [65, 59, 80], label: 'Crud Death Rate'},		
+	];
+
+	// DEATHRATEbarChartData = [
+	// 	{data: [65, 59, 80], label: 'Total Population'},
+	// 	{data: [28, 48, 40], label: 'Total Deaths'},
+	// 	{data: [28, 48, 40], label: 'Crud Death Rate'}
+	// ];
 
 
 	ngOnInit(): void {
@@ -16,3 +48,4 @@ export class DeathsStatComponent implements OnInit {
 
 
 }
+ 
