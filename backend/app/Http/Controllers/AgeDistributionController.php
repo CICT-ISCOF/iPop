f2a4dd35db531578d2fd37c240a4fea20850d5a4
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2020-12-20 23:33:10 +0800

    Update

---
+++ backend/app/Http/Controllers/AgeDistributionController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AgeDistribution;
+use App\Models\Log;
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class AgeDistributionController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $builder = AgeDistribution::getApproved();
+
+        foreach ($request->all() as $key => $value) {
+            $builder->where($key, $value);
+        }
+
+        return $builder->first();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $ageDistribution = AgeDistribution::where('year', $data['year'])
+            ->where('municipality', $data['municipality'])
+            ->where('barangay', $data['barangay'])
+            ->first();
+
+        if ($ageDistribution) {
+            $ageDistribution->update($data);
+            $ageDistribution->approval->update([
+                'message' => $request->user()->makeMessage('wants to update an age distribution.'),
+            ]);
+        } else {
+            $ageDistribution = AgeDistribution::create($data);
+            $ageDistribution->approval()->create([
+                'requester_id' => $request->user()->id,
+                'message' => $request->user()->makeMessage('wants to add an age distribution.'),
+            ]);
+        }
+
+        $ageDistribution->setApproved($request->user()->hasRole(Role::ADMIN));
+        Log::record("User create an age distribution.");
+
+        return $ageDistribution;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AgeDistribution  $ageDistribution
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AgeDistribution $ageDistribution)
+    {
+        return AgeDistribution::findApproved($ageDistribution->id)
+            ->first() ?: response('', 404);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AgeDistribution  $ageDistribution
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AgeDistribution $ageDistribution)
+    {
+        return response('', 404);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AgeDistribution  $ageDistribution
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AgeDistribution $ageDistribution)
+    {
+        $ageDistribution->makeDeleteRequest();
+
+        return response('', 204);
+    }
+}
---
+++ backend/app/Http/Controllers/TopPopulationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Log;
+use App\Models\TopPopulation;
+use Illuminate\Http\Request;
+
+class TopPopulationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return TopPopulation::getApproved()->first();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $topPopulation = TopPopulation::first();
+        if ($topPopulation) {
+            $topPopulation->update($data);
+            $topPopulation->approval->update([
+                'requester_id' => $request->user()->id,
+                'message' => $request->user()->makeMessage('wants to update a top population.')
+            ]);
+        } else {
+            $topPopulation = TopPopulation::create($data);
+            $topPopulation->approval()->create([
+                'requester_id' => $request->user()->id,
+                'message' => $request->user()->makeMessage('wants to add a top population.')
+            ]);
+        }
+
+        Log::record('User created a top population.');
+        return $topPopulation;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TopPopulation  $topPopulation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TopPopulation $topPopulation)
+    {
+        return TopPopulation::findApproved($topPopulation)
+            ->first() ?: response('', 404);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TopPopulation  $topPopulation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TopPopulation $topPopulation)
+    {
+        return response('', 404);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TopPopulation  $topPopulation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TopPopulation $topPopulation)
+    {
+        $topPopulation->makeDeleteRequest();
+
+        return response('', 204);
+    }
+}
