edc0b709328577424147d4e27843f4b2455634d0
Jamel <jamelyassin84@gmail.com>
2021-01-28 14:09:29 +0800

    age-dist-by-municipality

--- frontend/src/app/content/age-distribution-by-municipality/age-distribution-by-municipality.component.ts
+++ frontend/src/app/content/age-distribution-by-municipality/age-distribution-by-municipality.component.ts
@@ -1,4 +1,7 @@
+import { AgeDistributionServince } from './service.service';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-age-distribution-by-municipality',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgeDistributionByMunicipalityComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		private AgeDistributionServince : AgeDistributionServince
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.retrieve()
+	}
+
+
+	ageDistribution = {}
+	ageDistributions:any = []
+
+	create(){
+		this.AgeDistributionServince.create(this.ageDistribution).subscribe(data => { 
+			Swal.fire('Data has been added successfully','','success')
+			this.ngOnInit()
+		})
+	}
+
+	retrieve(){
+		this.AgeDistributionServince.retrieve().subscribe(data => { 
+			this.ageDistributions = data
+		})
+	}
+
+	update(ageDistribution){
+		this.AgeDistributionServince.update(ageDistribution).subscribe(data => { 
+			Swal.fire('Data has been updated successfully','','success')
+			this.ngOnInit()
+		})
+	}
+
+	deleteData(ageDistribution){
+		Swal.fire({
+			title: 'Are you sure you want to delete this data?',		
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonText: 'Delete',
+			cancelButtonText: 'Nope'
+		  }).then((result) => {
+			if (result.value) {
+				this.AgeDistributionServince.deleteData(ageDistribution['id']).subscribe(data => { 
+					Swal.fire('Data has been removed','','info')
+					this.ngOnInit()
+				})
+			} 
+		})	
+	}
+
+	editable = {}
+	edit(id){
+		this.editable[id] == true ?  this.editable[id] = false : this.editable[id] = true	
+	}
 
 }
+ 
