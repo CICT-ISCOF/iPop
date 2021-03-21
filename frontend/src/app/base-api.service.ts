85f0a9bea396c5c927c650ad10280fa7bd7f2088
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-03-21 22:01:55 +0800

    update

--- frontend/src/app/base-api.service.ts
+++ frontend/src/app/base-api.service.ts
@@ -1,26 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BaseAPIService {
+  constructor() {}
 
-  constructor() { }
+  public baseURL = 'http://localhost:8000/api';
 
-  	public baseURL = 'http://192.168.1.103:8000/api'
+  user = '';
+  token = '';
 
-	user =''
-	token = ''
-
-	getHeaders(){
-		this.user = JSON.parse(localStorage.getItem('user-data'))
-		this.token = this.user['token']
-		return new HttpHeaders({
-			'Accept':'application/json',
-			'Authorization' : 'Bearer '+ this.token,
-			'Content-Type':[]
-		})	
-  	}
-
-
+  getHeaders() {
+    this.user = JSON.parse(localStorage.getItem('user-data'));
+    this.token = this.user['token'];
+    return new HttpHeaders({
+      Accept: 'application/json',
+      Authorization: 'Bearer ' + this.token,
+      'Content-Type': [],
+    });
+  }
 }
