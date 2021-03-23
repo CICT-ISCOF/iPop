8d81ea78e3329fa898ef3f8d148afbb00010e273
Jamel Yassin <jamelyassin84@gmail.com>
2021-03-23 13:27:20 +0800

    merge

--- backend/database/migrations/2020_11_10_165753_create_articles_table.php
+++ backend/database/migrations/2020_11_10_165753_create_articles_table.php
@@ -15,9 +15,9 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('title',5000);
+            $table->string('title');
             $table->string('slug');
-            $table->text('body',9999999999);
+            $table->text('body');
             $table->timestamps();
         });
     }
