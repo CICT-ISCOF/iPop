3f32e289abd1528cd7a8fc74df0684a8b8e699d7
Jamel Yassin <jamelyassin84@gmail.com>
2021-03-25 18:40:30 +0800

    style

--- mobile/screens/rpfp/mpc-fdc/mpc-fdc.tsx
+++ mobile/screens/rpfp/mpc-fdc/mpc-fdc.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import TopPadding from '../../../shared/top-padding/top-padding';
+import MPCFDCsSelects from './mpc-fdc-select';
+import Colors from '../../../constants/Colors';
+import useColorScheme from '../../../hooks/useColorScheme';
+import { ScrollView } from 'react-native-gesture-handler';
+import BackContainer from '../../../shared/back-container/back-container';
+
+import styles from './mpc-fdc.style';
+
 
 export default function MPCFDC() {
     const colorScheme = useColorScheme();
     return (
-        <View style={[styles.container, { padding: 0 }]}>
+        <View style={[ styles.container, { padding: 0 } ]}>
             <TopPadding />
             <ScrollView
                 showsVerticalScrollIndicator={false}
                 style={[
                     styles.container,
                     {
-                        backgroundColor: Colors[colorScheme].background,
+                        backgroundColor: Colors[ colorScheme ].background,
                     },
                 ]}>
                 <BackContainer hidden={true} />
                 <Text
-                    style={[styles.menu, { color: Colors[colorScheme].text }]}>
+                    style={[ styles.menu, { color: Colors[ colorScheme ].text } ]}>
                     Multi-purpose Counseling and Family Development Center
                 </Text>
                 <MPCFDCsSelects />
@@ -26,11 +34,3 @@ export default function MPCFDC() {
         </View>
     );
 }
-import MPCFDCsSelects from './mpc-fdc-select';
-import Colors from '../../../constants/Colors';
-import useColorScheme from '../../../hooks/useColorScheme';
-import { ScrollView } from 'react-native-gesture-handler';
-import SearchNav from '../../main/home/components/search/search';
-import BackContainer from '../../../shared/back-container/back-container';
-
-import styles from './mpc-fdc.style';
