8e45b162c0873382836f58c4e935b2665dcb26f2
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-20 18:29:14 +0800

    extras approvals

--- backend/app/Models/PersonnelDirectory.php
+++ backend/app/Models/PersonnelDirectory.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\HasApproval;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PersonnelDirectory extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApproval;  
     
     protected $fillable = ['type', 'name', 'position', 'phone', 'email'];
 }
